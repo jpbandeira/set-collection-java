e10317e11e11a90c0445d9d7b2e2bf32c656b634
JPBandeiraLima <jpbandeiralima@gmail.com>
2020-11-24 16:27:13 -0300

    Inclusões de arvora como estrutura.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,31 @@
-import java.io.Serializable;
-import java.util.LinkedHashSet;
+
 
 public class Main {
     public static void main(String[] args) {
+    	/*
+        OrderedSet<Integer> stringSet = new OrderedSet<>();
 
-        GenericObject genericObject = new GenericObject(1, new Object());
-        GenericObject genericObject2 = new GenericObject(2, new Object());
-        GenericObject genericObject3 = new GenericObject(3, new Object());
-
-        Set<Integer> stringSet = new Set<>();
-
-        stringSet.addToList(1);
-        stringSet.addToList(2);
-        stringSet.addToList(3);
-        stringSet.addToList(2);
+        stringSet.add(1);
+        stringSet.add(4);
+        stringSet.add(3);
+        stringSet.add(2);
+        
+        stringSet.remove(2);
+        
+        stringSet.add(7);
+        stringSet.add(5);
+        stringSet.add(5);
 
         System.out.println(stringSet.toString());
-
+        */
+    	
+    	AvlSet s = new AvlSet();
+    	s.inserir(2);
+    	s.inserir(3);
+    	s.inserir(4);
+    	s.inserir(5);
+    	s.inserir(6);
+    	s.inserir(5);
+    	s.preOrdem();
     }
 }
