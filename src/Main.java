295bac82579278df94f7effc48a8102b9de1fffd
JPBandeiraLima <jpbandeiralima@gmail.com>
2020-11-24 17:40:26 -0300

    Inclusão de metodos para AvlSet

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-
+import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,14 +19,38 @@ public class Main {
 
         System.out.println(stringSet.toString());
         */
-    	
-    	AvlSet s = new AvlSet();
+		long tempoInicial = System.currentTimeMillis();
+		AvlSet<Integer> alvT = new AvlSet<>();
+
+		alvT.add(3);
+		alvT.add(9);
+		alvT.add(89);
+		alvT.add(17);
+		alvT.add(5);
+		alvT.add(4);
+		alvT.add(3);
+		alvT.add(1);
+
+		alvT.print();
+		System.out.println(alvT.contains(3));
+		long tempoFinal = System.currentTimeMillis();
+
+		Date calendar = new Date(tempoInicial);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(calendar);
+		System.out.println(cal);
+
+		System.out.println();
+		System.out.println(tempoFinal);
+		System.out.println(tempoInicial - tempoFinal);
+ /*
+    	AvlTree s = new AvlTree();
     	s.inserir(2);
     	s.inserir(3);
     	s.inserir(4);
     	s.inserir(5);
     	s.inserir(6);
     	s.inserir(5);
-    	s.preOrdem();
+    	s.preOrdem();*/
     }
 }
